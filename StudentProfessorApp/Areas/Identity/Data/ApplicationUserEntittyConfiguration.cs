ac38f6a9ae549edb4852cd7ab193263142ed6ea6
Milos Milinkov <milos300399@gmail.com>
2022-10-07 13:58:16 +0200

    Login_SingUp
    Added login and signup functionality.

---
+++ StudentProfessorApp/Areas/Identity/Data/ApplicationUserEntittyConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace StudentProfessorApp.Areas.Identity.Data
+{
+    internal class ApplicationUserEntittyConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            builder.Property(u => u.FirstName).HasMaxLength(255);
+            builder.Property(u => u.LastName).HasMaxLength(255);
+        }
+    }
+}
---
+++ StudentProfessorApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentProfessorApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
