1ca30379146b9b789216b268f13968c887721d75
Milos Milinkov <milos300399@gmail.com>
2022-10-08 12:21:59 +0200

    Editing Roles
    Added the options to edit aand view all roles aswell to see the future users with given roles.

--- StudentProfessorApp/Controllers/AdministrationController.cs
+++ StudentProfessorApp/Controllers/AdministrationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using StudentProfessorApp.Areas.Identity.Data;
 using StudentProfessorApp.ViewModels;
 
 namespace StudentProfessorApp.Controllers
@@ -7,9 +8,12 @@ namespace StudentProfessorApp.Controllers
     public class AdministrationController: Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
-        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        private readonly UserManager<ApplicationUser> userManager;
+        public AdministrationController(RoleManager<IdentityRole> roleManager,
+                                        UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
         [HttpGet]
         public IActionResult CreateRole()
@@ -43,6 +47,58 @@ namespace StudentProfessorApp.Controllers
         {
             var roles = roleManager.Roles;
             return View(roles);
+        }
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage=$"Role with Id={id} could not be found!";
+                return View("NotFound");
+            }
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name,
+            };
+            foreach (var user in userManager.Users)
+            {
+                if(await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+
+                }
+
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await roleManager.FindByIdAsync(model.Id);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id={model.Id} could not be found!";
+                return View("NotFound");
+            }
+            else
+            {
+                role.Name = model.RoleName;
+                var result=await roleManager.UpdateAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
+            
+           
+            
         }
     }
 }
